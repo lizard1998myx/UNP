4291b4cbcb30cf6e8944f3911cca13e51d2f1198
Sgt.Lizard <lizard1998myx@gmail.com>
2019-08-09 14:10:13 +0800

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -1,5 +1,5 @@
 from IO import SearcherIO, LoginerIO
-
+from Core import Account
 
 class Interface:
     def __init__(self):
@@ -12,14 +12,14 @@ class Interface:
 
     @staticmethod
     def version():
-        version = "V5.0 - 20190808"
+        version = "V5.1 - 20190808"
         return version
 
     @staticmethod
     def history():
         history = """[Version history]
 V0.10 (20181230) Searcher 1.0
-V0.50 (20190103) Loginer 1.0
+V0.50 (20190103) Loginer  1.0
 V0.60 (20190624) account data update
 V1.00 (20190625) Combine Searcher and Loginer
 V1.10 (20190625) Network tester
@@ -32,13 +32,15 @@ V1.34 (20190626) command line interface
 V2.00 (20190626) stable version
 V2.10 (20190626) Searcher 2.2 (better search)
 V2.22 (20190626) Automode update
-V3.00 (20190626) Loginer 2.0 (new accounts)
+V3.00 (20190626) Loginer  2.0 (new accounts)
                  silent and developer modes
-V3.10 (20190626) Loginer 2.1 (customization)
+V3.10 (20190626) Loginer  2.1 (customization)
 V4.00 (20190627) Searcher 3.0 (password mode)
 V4.03 (20190701) tester debug and icon
-V4.10 (20190802) Loginer 3.0 (timer)
+V4.10 (20190802) Loginer  3.0 (timer)
 V5.00 (20190808) Object Oriented update
+V5.20 (20190809) Searcher 5.0 (special modes)
+                 error fix Core
 """
         return history
 
@@ -49,6 +51,7 @@ V5.00 (20190808) Object Oriented update
         print(self.info())
         print(self.history())
         print(self.version())
+        Account.test()
         if input("into Loginer? (enter to skip):") != "":
             LoginerIO.example()
         if input("into Searcher? (enter to skip):") != "":
