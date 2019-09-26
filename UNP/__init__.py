0e2172d04bedbdb90b519e40f025c0bd144b43ba
Sgt.Lizard <lizard1998myx@gmail.com>
2019-09-26 12:45:09 +0800

    Update __init__.py

--- UNP/__init__.py
+++ UNP/__init__.py
@@ -1,5 +1,14 @@
 from UNP.Interface import Interface
+from UNP.Application import Loginer
 
 
 def activate():
     Interface().keyboard()
+
+
+def activate_shortcut():
+    Loginer().active()
+
+
+def activate_passive():
+    Loginer().passive()
